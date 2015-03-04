e9ffac49635f6b7a3bc7c426821511fcfeea2802
MigueVeliz <miguelveliz91@gmail.com>
2015-03-04 15:18:36 -0500

    Sound
    Hadouken sound added

--- js/app.js
+++ js/app.js
@@ -8,11 +8,20 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	}).mousedown(function() {
-		console.log("mosuedown");
+		playHadouken();
+		// console.log("mosuedown");
 		// play hadouken sound
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		// $('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+		  {'left': '1020px'},
+		  	500,
+		  	function() {
+		    	$(this).hide();
+		    	$(this).css('left', '520px');
+		 	 }
+			);
     	// show hadouken and animate it to the right of the screen
 	}).mouseup(function() {
 		console.log("mouse up");
@@ -22,6 +31,11 @@ $(document).ready(function() {
 	});
 
 
-
-
 });//end of document.ready
+
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
